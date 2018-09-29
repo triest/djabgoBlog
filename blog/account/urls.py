bd2ce7b4412c31acc439e90383ddc8a79f3989af
triest <you@example.com>
2018-09-29 11:07:45 +0300

    logout action work article dateils not working

--- blog/account/urls.py
+++ blog/account/urls.py
@@ -9,6 +9,7 @@ app_name='accounts'
 urlpatterns = [
     path('singup', views.singup_view,name="singup"),
     path('login', views.login_view, name="login"),
+    path('logout',views.logout_view,name="logout")
  #   path('login', views.login_view,name="singup"),
 
 ]
--- blog/account/views.py
+++ blog/account/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login,logout;
 # Create your views here.
 
 def singup_view(request):
@@ -10,9 +11,16 @@ def login_view(request):
     if request.method=='POST':
         form=AuthenticationForm(data=request.POST);
         if(form.is_valid()):
+            user=form.get_user();
+            login(request,user)
             return redirect('articles:list')
 
     else:
         form=AuthenticationForm();
         return render(request,'account/login.html',{'form':form})
 
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('articles:list')
