8cefc861ef66282334e1e8bcb32e80c4443e6c83
triest <you@example.com>
2018-10-04 15:40:52 +0300

    sulect sunscribed users

--- blog/subscribe/views.py
+++ blog/subscribe/views.py
@@ -2,8 +2,9 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from inspect import getmembers
 from pprint import pprint
-
+from .models import Subscribe
 from django.forms import modelform_factory
+from .models import Subscribe
 from . import forms
 from django.contrib.auth.models import User;
 
@@ -11,10 +12,11 @@ from django.contrib.auth.models import User;
 @login_required(login_url="/account/login")
 def main(request):
     current_user = request.user
-    #print(getmembers(current_user))
-    #print (vars(current_user))
-    #subscrive=Subscribe.objects.all();
-   # subscribe=Subscribe.objects.all();
- #   form = forms.SunscribeForm()
-    return render(request,'test.html')
+    #получаем инстанс subscribe авторизованного пользователя
+    subscribe=Subscribe.objects.get(owner=current_user)
+    users = subscribe.subscription.all()
+
+    #
+
+    return render(request,'test.html',{'users':users})
 
